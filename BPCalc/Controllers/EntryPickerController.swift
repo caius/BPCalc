ed07d732c4858802d86ead8408f4efa6af687fd0
Caius Durling <dev@caius.name>
2018-01-29 01:23:40 +0000

    Tweak up PickerViewController for stuffs

--- BPCalc/Controllers/EntryPickerController.swift
+++ BPCalc/Controllers/EntryPickerController.swift
@@ -8,15 +8,33 @@
 
 import UIKit
 
-//
+// Entry datapicker has pretty obvious requirements:
+// * Enter both Systolic & Diastolic values
+// * Both have values from 1-300
+// * Selection defaults to 120/80 unless told otherwise
 class EntryPickerController: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
+
+  var pickerView: UIPickerView
+
+  override init() {
+    self.pickerView = UIPickerView()
+    super.init()
+
+    pickerView.delegate = self
+    pickerView.dataSource = self
+  }
 
   func numberOfComponents(in pickerView: UIPickerView) -> Int {
     return 2
   }
 
   func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-    return 1
+    return 300
+  }
+
+  // Rows 0..299 generate strings "1".."300"
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return String(row + 1)
   }
 
 }
