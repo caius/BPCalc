ecafb211495f6d5e4e7aeeffe2a71615888bc1ce
Caius Durling <dev@caius.name>
2018-01-29 01:12:40 +0000

    Only run tailor during Analyze phase

--- Scripts/tailor.sh
+++ Scripts/tailor.sh
@@ -6,8 +6,10 @@ set -o nounset
 set -o pipefail
 set -o noclobber
 
-if hash tailor 2>/dev/null; then
-  tailor
-else
-  echo "warning: Please install Tailor from https://tailor.sh"
+if [ "${CONFIGURATION}" = "Analyze" ]; then
+  if hash tailor 2>/dev/null; then
+    tailor
+  else
+    echo "warning: Please install Tailor from https://tailor.sh"
+  fi
 fi
